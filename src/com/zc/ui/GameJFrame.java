2d5960c13e7e50b1918b582d49cb9f1ba761ae1d
Chen Zhao <zhaochen020410@gmail.com>
2025-01-15 19:46:45 -0500

    Load pictures to Game

--- src/com/zc/ui/GameJFrame.java
+++ src/com/zc/ui/GameJFrame.java
@@ -6,13 +6,37 @@ public class GameJFrame extends JFrame {
     // All codes about main frame are written here
     public GameJFrame(){
         // Initialize the game frame
-        init();
+        initJFrame();
 
         // Initialize the menu
         initJMenuBar();
 
+        // Initialize the pictures
+        initImage();
+
         // Make this frame visible
         this.setVisible(true);
+    }
+
+    private void initImage() {
+        // Numbers of one icon
+        int number = 1;
+        // Outer loop, to put icons in a line
+        for (int i = 0; i < 4; i++) {
+            // Inner loop to put icons through columns
+            for (int j = 0; j < 4; j++) {
+                // Create a JLabel (container of all icons)
+                JLabel jLabel = new JLabel(new ImageIcon("C:\\Users\\zc\\IdeaProjects\\PuzzleGame\\image\\animal\\animal3\\"+ number+".jpg"));
+                // Assign the location of the Image
+                jLabel.setBounds(105 * j, 105 * i, 105, 105);
+                // getContentPane -> acquire the invisible container
+                this.getContentPane().add(jLabel);
+                // Load next icon
+                number++;
+            }
+
+        }
+
     }
 
     private void initJMenuBar() {
@@ -46,7 +70,7 @@ public class GameJFrame extends JFrame {
         this.setJMenuBar(jMenuBar);
     }
 
-    private void init() {
+    private void initJFrame() {
         // Set width and height
         this.setSize(603,680);
         // Set title for frame
@@ -57,6 +81,10 @@ public class GameJFrame extends JFrame {
         this.setLocationRelativeTo(null);
         // Set default close mode
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // Cancel default layout(in the middle),
+        // the icon follows x-y coordinator as you cancel this
+        this.setLayout(null);
+
     }
 
 }
